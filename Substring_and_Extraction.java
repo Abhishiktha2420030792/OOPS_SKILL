7420db1615115fc9a9cede98527601699ff6580c
Abhishiktha2420030792 <2420030792@klh.edu.in>
2025-07-30 10:15:57 +0530

    Add files via upload

---
+++ Substring_and_Extraction.java
@@ -0,0 +1,10 @@
+package oops1;
+
+public class Substring_and_Extraction {
+
+	public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.substring(5));         
+        System.out.println(s.substring(2, 7));       
+    }
+}
