4d1a4a5427e778e2c54b55c2c014c41ac4d648fb
Abhishiktha2420030792 <2420030792@klh.edu.in>
2025-07-30 10:20:08 +0530

    Add files via upload

---
+++ Trimming_and_Whitespace.java
@@ -0,0 +1,9 @@
+package oops1;
+
+public class Trimming_and_Whitespace {
+	public static void main(String[] args) {
+        String s = "  Hello  ";
+        System.out.println("[" + s.trim() + "]");     
+    }
+
+}
