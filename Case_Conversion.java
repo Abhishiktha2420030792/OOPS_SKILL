b07b0fb11bb819a59b8ac4c91ee588943225f89f
Abhishiktha2420030792 <2420030792@klh.edu.in>
2025-07-30 10:18:43 +0530

    Add files via upload

---
+++ Case_Conversion.java
@@ -0,0 +1,10 @@
+package oops1;
+
+public class Case_Conversion {
+	public static void main(String[] args) {
+        String s = "hello world";
+        System.out.println(s.toUpperCase());   
+        System.out.println(s.toLowerCase());   
+    }
+
+}
